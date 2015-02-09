dca662859426f4ba43c0c88243b930c6e88e2869
nicola <nicola.cavallini@unipv.it>
2015-02-09 17:24:45 +0100

    few observations

--- modules/reference_shape_function.py
+++ modules/reference_shape_function.py
@@ -15,19 +15,23 @@ class ReferenceShapeFuncion:
         y = quad[:1]
 
         if self.degree==1:
+            #TODO: change to numpy array
             value_list = [1.-x-y , x , y]
-        if self.degree==2:      
+        if self.degree==2:
+            #TODO: change to numpy array
             value_list = [2*(x**2+y**2)+4*x*y-3*(x+y)+1 , 2*(x**2)-x , 2*(y**2)-y , -4*(x**2)-4*x*y+4*x , 4*x*y , -4*(y**2)-4*x*y+4*y ]
         return value_list
 #==============================================================================
     def eval_basis_gradients(self,quad):
 
-        x = quad[:0]
+        x = quad[:0]# quadrature is going to become a class this line will read: x = quad.x
         y = quad[:1]
 
-        if self.degree==1:        
+        if self.degree==1:
+            #TODO: change to numpy array
             grad_list = [[-1,-1],[1,0],[0,1]]      
         if self.degree==2:
+            #TODO: change to numpy array
             grad_list =[[4*x+4*y-3 , 4*y+4*x-3],[4*x-1 , 0],[0 , 4*y-1],[-8*x-4*y+4 , 4*x],[4*y , 4*x],[4*y , -8*y-4*x+4]]
              
         # for every quadrature point we create
