cbc6290b522ae7717302a47f0f147de3969adcc7
michele annese <m.annese@unibs.it>
2015-02-11 12:09:53 +0100

    classe mapping

--- modules/mapping.py
+++ modules/mapping.py
@@ -5,11 +5,13 @@ Created on Sat Feb  7 15:14:45 2015
 @author: nicola
 """
 
+from mesh import Mesh
+
 class Mapping:
     def __init__(self,mesh):
-        self.edge_topo # connettivity matrix corresponding only to the 
-                       # edge nodes of the mesh 
-                       # (deg2: exclude mid segment nodes)
-        self.x # edge nodes
-        self.y # edge nodes
+        self.edge_topo =  mesh.topo[:0:3]        # connettivity matrix corresponding only to the 
+                                                 # edge nodes of the mesh 
+                                                 # (deg2: exclude mid segment nodes)
+        self.x = mesh.x                                    # edge nodes
+        self.y = mesh.y                                  # edge nodes
         return
