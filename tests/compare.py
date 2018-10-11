cb9951ada0b4658830b751e25e74138ba6f5a7ad
sebwolf-de <sebwolf@web.de>
2018-10-11 11:19:28 +0200

    Changed output of parameter handler simulation info

--- tests/compare.py
+++ tests/compare.py
@@ -9,7 +9,7 @@ def l2_norm(M,g):
     l2_g = mth.sqrt(l2_g)
     return l2_g
 
-results_dir = 'results/BDF_Convergence_Analysis/'
+results_dir = 'results/BDF_Convergence_Analysis_Cavity/'
 
 #Get mass matrix, all time integrations were being executed on the same mesh
 filename = results_dir+'mesh'
@@ -34,8 +34,14 @@ mass_matrix = sparse.vstack([
     sparse.hstack( [sparse.csr_matrix((ndofs_u,ndofs_u)), mass_1D] )
     ])
 
+stiffness_1D = assemble.gradu_gradv_p1(topo_u,x_u,y_u)
+stiffness_matrix = sparse.vstack([
+    sparse.hstack( [stiffness_1D, sparse.csr_matrix((ndofs_u,ndofs_u)) ] ),
+    sparse.hstack( [sparse.csr_matrix((ndofs_u,ndofs_u)), stiffness_1D] )
+    ])
 
-input_name = results_dir+'Reference'
+
+input_name = results_dir+'reference'
 f = file(input_name,"rb")
 u_reference = np.load(f)
 p_reference = np.load(f)
@@ -43,10 +49,10 @@ xs_reference = np.load(f)
 ys_reference = np.load(f)
 f.close()
 
-err_BDF1 = np.zeros(6)
-err_BDF2 = np.zeros(6)
+err_BDF1 = np.zeros(5)
+err_BDF2 = np.zeros(5)
 
-for k in range(1,7):
+for k in range(1,6):
     input_name = results_dir+'BDF1_dt=1_'+str(2**k)
     f = file(input_name,"rb")
     u_BDF1 = np.load(f)
@@ -55,6 +61,8 @@ for k in range(1,7):
     ys_BDF1 = np.load(f)
     f.close()
 
+    #err_BDF1[k-1] = mth.sqrt(l2_norm(mass_matrix, u_BDF1 - u_reference)**2
+    #                        + l2_norm(stiffness_matrix, u_BDF1 - u_reference)**2)
     err_BDF1[k-1] = l2_norm(mass_matrix, u_BDF1 - u_reference)
 
     input_name = results_dir+'BDF2_dt=1_'+str(2**k)
@@ -65,9 +73,11 @@ for k in range(1,7):
     ys_BDF2 = np.load(f)
     f.close()
 
+    #err_BDF2[k-1] = mth.sqrt(l2_norm(mass_matrix, u_BDF2 - u_reference)**2
+    #                       + l2_norm(stiffness_matrix, u_BDF2 - u_reference)**2)
     err_BDF2[k-1] = l2_norm(mass_matrix, u_BDF2 - u_reference)
 
 print 'BDF1 Error: '+str(err_BDF1)
 print 'BDF2 Error: '+str(err_BDF2)
-print 'Error decay BDF1: '+str(np.divide(err_BDF1[0:5], err_BDF1[1:6]))
-print 'Error decay BDF2: '+str(np.divide(err_BDF2[0:5], err_BDF2[1:6]))
+print 'Error decay BDF1: '+str(np.divide(err_BDF1[0:4], err_BDF1[1:5]))
+print 'Error decay BDF2: '+str(np.divide(err_BDF2[0:4], err_BDF2[1:5]))
