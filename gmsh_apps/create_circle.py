e9f7261d3ad486833e33c240322d604415aa9a00
sebwolf-de <sebwolf@web.de>
2018-10-19 10:19:31 +0200

    Create some nice circle meshs

---
+++ gmsh_apps/create_circle.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import math
+
+n = 8
+for k in range(0,n):
+    print 'Point('+str(k+1)+') = { '+str(math.cos(2*math.pi*k/n))+', '+str(math.sin(2*math.pi*k/n))+', 0, 1.0};'
+
+for k in range(0,n):
+    print 'Line('+str(k+1)+') = {'+str(k+1)+', '+str(k+2)+'};'
+
+s = 'Line Loop(1) = {'
+for k in range(0,n):
+    s += str(k+1)+', '
+s += '};'
+print s
+
+print 'Plane Surface(1) = {1};'
