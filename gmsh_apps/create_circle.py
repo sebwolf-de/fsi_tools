6da06aac9649fa6cb52f01ef5d870ae9a60b1c2e
sebwolf-de <sebwolf@web.de>
2018-12-05 15:24:28 +0200

    some new meshes

--- gmsh_apps/create_circle.py
+++ gmsh_apps/create_circle.py
@@ -2,17 +2,17 @@
 
 import math
 
-n = 28
+n = 64  
 for k in range(0,n):
     print('Point('+str(k+1)+') = { '+str(math.cos(2*math.pi*k/n))+', '+str(math.sin(2*math.pi*k/n))+', 0, 1.0};')
 
 for k in range(0,n):
-    print('Line('+str(k+1)+') = {'+str(k+1)+', '+str(k+2)+'};')
+    print('Line('+str(k+1)+') = {'+str(k+1)+', '+str((k+1)%n+1)+'};')
 
 s = 'Line Loop(1) = {'
-for k in range(0,n):
+for k in range(0,n-1):
     s += str(k+1)+', '
-s += '};'
+s += str(n) + '};'
 print(s)
 
 print('Plane Surface(1) = {1};')
